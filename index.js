01eadead8165a410c516be64b2ebc3f02d75aefe
NeilBanerjee2002 <neilbanerjee.22.5.2002@gmail.com>
2025-05-01 21:06:33 +0530

    added for loop to render product json

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 import { products } from "./DB/product.js";   
 
-console.log(products);
+const parentContainer = document.getElementById("product");
+
+for (let product of products){
+    const cardContainer = document.createElement("div");
+    cardContainer.classList.add("card",
+        "card-vertical",
+        "d-flex",
+        "direction-column",
+        "relative",
+        "shadow",
+    );
+    
+    const cardImage = document.createElement("img");
+    cardImage.classList.add("card-image");
+    cardImage.src = product.img;
+    
+    cardContainer.appendChild(cardImage);
+    parentContainer.appendChild(cardContainer);
+}   
+
